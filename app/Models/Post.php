6ac10c2ecfde1a541968f7f112bdd16e7427fd89
hassanms <hassansid95@gmail.com>
2021-04-13 18:58:50 +0500

    Adding Posts to the Database & Many to Many Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class Post extends Model
 {
     use HasFactory;
-
-    protected $guarded = [];
     
     public function user(){
         return $this->belongsTo(User::class);
